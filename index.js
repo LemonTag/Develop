4334b417be4901e3e8d903b9f73d360efcc8029b
Keegan Micu <LemonTag73@gmail.com>
2024-04-23 17:04:55 -0400

    working on the making the if statements on the licenses

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs')
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "description",
+        message: "what is a description"
+    },
+
+    {
+        type: "list",
+        name: "description",
+        message: "what is a description",
+        choices:['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'NONE']
+    },
+    
+    {
+        type: "input",
+        name: "description",
+        message: "what is a description"
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) { 
+    fs.writeFile("readme.md", string,(err)=>{
+        if (err) throw err;
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() { 
+    //ask tge questions
+    inquirer.prompt(questions).then((anwsers) =>{
+        //pass the answers to the big string
+        const string = generateMarkdown(anwsers)
+        // write the file with that string
+       writeToFile("readme.md", string)
+    })
+   
+   
+}
 
 // Function call to initialize app
 init();
 
-
-const inquirer = require('inquirer')
-
-inquirer
-    .prompt([{
-        type: "input",
-        name: "projectname",
-        message: "what's your project name.",
-    }
-  
-    {
-        type: "list",
-        name: "favoriteTech",
-        message: "what's your favorite tech.",
-        chocies: ["HTML", "CSS", "Node", "Javascript"]
-
-    }
-    ])
-    
-    .then((projectName, favortieTech) => {
-        // Use user feedback for... whatever!!
-        (console.log(projectName, favortieTech))
-    })
-    
-    .catch((error) => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            // Something else went wrong
-        }
-    });
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,43 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { //img.shields.io/badge/javascript-yellow, img.shields.io/badge/nodejs-green
+  if (license !== none) {
+
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+  if (license !== none) {
+    return license`[license](https://github.com/LemonTag/Develop)`
+  }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+if (license !== none) {
+  function renderLicenseSection(license) { license }
+  return `
+  ## license
+  this project is license with ${license}`
+}
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `# ${data.description}
+  ## description
+  ${data.description}
+${renderLicenseSection(data.license)}
+  ###Table of Contents
+*[installation](#installation)
+*[installation](#installation)
+*[installation](#installation)
+  ${(renderLicenseSection(date.license))}
 `;
 }
 
